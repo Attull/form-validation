c8f62819bc03000f8b78cb1a297eeb7afe08a9db
attull <atul852798@gmail.com>
2023-08-18 17:12:38 +0530

    implememt form validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const initialValues = { username: "", number: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({})
+
+
+  const handleChange = (e) =>{
+    let  {name, value} = e.target
+
+    console.log("name",name)
+    setFormValues({...formValues, [name] : value})    
+    console.log(formValues)
+  }
+
+  const validate = (e) =>{
+    e.preventDefault()
+    const error = {}
+
+    if(formValues.username == ""){
+        error.username = "Please provide name"
+    } else if(formValues.username.length <3){
+      error.username = "Please provide complete name"
+    } 
+
+    
+    if(formValues.number >10){
+      error.number = "Provide valid number"
+    } else if(formValues.number == ""){
+      error.number = "provide number"
+    }
+
+    setFormErrors(error)
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1>Forms</h1>
+          <form onSubmit={(e) => validate(e)}>
+              <div className='inp'>
+                  <lable>Name</lable>
+                  <input 
+                    type="text"
+                    placeholder='Enter your name'
+                    name="username"
+                    value={formValues.username}
+                    onChange={(e) => handleChange(e)}
+                    />
+                    <p>{formErrors.username}</p>
+              </div>
+              <div className='inp'>
+                  <lable>Phone number</lable>
+                  <input 
+                    type="text"
+                    name="number"
+                    placeholder='Enter your number'
+                    value={formValues.number}
+                    onChange={handleChange}
+                    />
+                    <p>{formErrors.number}</p>
+              </div>
+              <div className='inp'>
+                  <lable>Passsword</lable>
+                  <input 
+                    type="password"
+                    name="password"
+                    placeholder='Enter your password'
+                    value={formValues.password}
+                    onChange={handleChange}
+                    />
+              </div>
+              <button type="btn">Submit</button>
+          </form>
     </div>
   );
 }
